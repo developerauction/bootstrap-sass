70926de2246a199a584822c805d611df61e85b23
Curtis Gagliardi <curtis@developerauction.com>
2013-07-18 12:13:37 -0700

    Update the update-bootstrap script and update to latest bootstrap3 scss from jlong.

--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -9,19 +9,25 @@ mkdir -p $ROOT/stylesheets/bootstrap
 cp -r $TMP/lib/* $ROOT/stylesheets/bootstrap
 # Copy js/ to javascripts/bootstrap
 cp -r $TMP/js/* $ROOT/javascripts/bootstrap
+
+# CURTIS: img folder no longer exists (it was related to glyphicons which aren't bundled with BS3)
 # Copy img/ to images/
-cp -r $TMP/img/* $ROOT/images
+#cp -r $TMP/img/* $ROOT/images
+
 # Copy fonts/ to fonts/
 cp -r $TMP/fonts/* $ROOT/fonts
 # Remove tests
 rm -r $ROOT/javascripts/bootstrap/tests
 
+# CURTIS: Glyphicons are no longer bundled with BS3
 # Patch the asset-url in _glyphicons.scss
-patch -f vendor/assets/stylesheets/bootstrap/_glyphicons.scss < asseturl.patch
-rm $ROOT/stylesheets/bootstrap/_glyphicons.scss.orig
+#patch -f vendor/assets/stylesheets/bootstrap/_glyphicons.scss < asseturl.patch
+#rm $ROOT/stylesheets/bootstrap/_glyphicons.scss.orig
 
 # Patch paths in bootstrap.scss and responsive.scss
-sed -i .bak 's_@import \"_@import \"bootstrap/_g' $ROOT/stylesheets/bootstrap/bootstrap.scss
+
+# CURTIS: sed is different on osx and linux, needs to look like this for linux
+sed -i.bak 's_@import \"_@import \"bootstrap/_g' $ROOT/stylesheets/bootstrap/bootstrap.scss
 rm $ROOT/stylesheets/bootstrap/*.bak
 
 rm -rf $TMP
